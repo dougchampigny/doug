03ad22234abfb6a9d949be1f0ccd3d62e5bab386
dougchampigny <dougchampigny@yahoo.com>
2015-01-07 20:41:28 -0500

    Create Francis.php

---
+++ Francis.php
@@ -0,0 +1,20 @@
+<?php
+	class Francis {	
+		
+		public function yo($input)
+		{
+			if($input == 'Did you finish your homework?'){
+				return 'Sure';
+			}
+			elseif($input == 'GOOOAAALLL'){
+				return 'Chill';
+			}
+			elseif($input == ''){
+				return 'See if I care!';
+			}
+			else{
+				return 'Whatevs.';
+			}		
+		}
+	}
+?>
